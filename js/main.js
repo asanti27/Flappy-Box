c96d27aafde11bf3ca83d60a3caadd7838222d40
angyasantin@gmail.com <>
2014-08-14 18:03:50 +0000

    intial.g

--- js/main.js
+++ js/main.js
@@ -31,14 +31,60 @@ game.physics.arcade.enable(this.bird);
 
 this.bird.body.gravity.y=1000;
 
+var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+spaceKey.onDown.add(this.jump, this);
 
 
 
+this.pipes = game.add.group();
 
-  
+this.pipes.enableBody = true;
+
+this.pipes.createMultiple(20, 'pipe');
+
+this.timer = game.time.events.loop(1500,this.addRowOfPipes, this);  
+
+
 },
 update:function(){
- } 
-};
+if (this.bird.inWorld == false){
+  this.restartGame();
+}
+ },
+restartGame:function(){
+  game.state.start ('main');
+  
+ },
+ addOnePipe:function (x,y) {
+ var pipe = this.pipes.getFirstDead();
+ 
+ pipe.reset(x,y);
+ pipe.body.velocity.x=-200;
+ pipe.checkWorldBounds=true;
+ pipe.outOfBoundsKill=true;
+
+ },
+ addRowOfPipes: function () {
+ var hole=Math.floor(Math.random()*5)+1;
+ 
+ for (var i = 0; i<8; i++)
+  if (i !=hole && i != hole+1) {
+  this.addOnePipe(400, i*60+10);
+  
+
+  
+} 
+},
+ 
+ jump:function(){
+   this.bird.body.velocity.y=-350;
+ }
+ 
+ 
+ 
+ 
+ 
+ };
 game.state.add('main',mainState);
 game.state.start ('main');
