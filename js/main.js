140f5275505469afeafa04d2e86555ab0a187aac
angyasantin@gmail.com <>
2014-08-14 15:46:41 +0000

    INITIAL.G

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-//Initialize Phaser Engine. Create a 400x400px game!//
 
-var game= new Phaser. Game(400,400,Phser,AUTO,'gameDiv')
+
+var game= new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
 
 
 
@@ -16,8 +16,25 @@ var mainState={
   
   
   game.stage.backgroundColor="#71c5cf";
+  
+  game.load.image('bird', 'assets/bird.png');
+  
+   game.load.image('pipe', 'assets/pipe.png');
 },
 create:function(){
+
+game.physics.startSystem(Phaser.Physics.ARCADE);
+
+this.bird = this.game.add.sprite(100,245, 'bird');
+
+game.physics.arcade.enable(this.bird);
+
+this.bird.body.gravity.y=1000;
+
+
+
+
+
   
 },
 update:function(){
